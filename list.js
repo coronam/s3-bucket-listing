27982dac77cf153d447361043cc570b915a22f76
Marco Corona <marco.corona@coveros.com>
2018-06-25 14:27:35 -0400

    forgot +

--- list.js
+++ list.js
@@ -231,7 +231,7 @@ function prepareTable(info) {
     // strip off the prefix
     item.keyText = item.Key.substring(prefix.length);
     if (item.Type === 'directory') {
-      console.log("item.key in jquery: " item.Key);
+      console.log("item.key in jquery: " + item.Key);
       if (S3BL_IGNORE_PATH) {
         item.href = location.protocol + '//' + location.hostname +
                     location.pathname + '?prefix=' + item.Key;
